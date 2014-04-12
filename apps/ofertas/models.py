0236e3dbc69848723f4ec57975451fb0bb7e36fd
larribas <lorenzo.arribas@me.com>
2014-04-12 14:59:17 +0200

    Implement Offer models

---
+++ apps/ofertas/models.py
@@ -0,0 +1,75 @@
+from apps.base import enums
+from apps.base.models import ConocimientoTecnico, SectorDelMercado, Idioma
+from apps.usuarios.models import Empresa, Profesor, Estudiante
+from django.db import models
+
+
+class Oferta(models.Model):
+    #Características de la oferta
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=600)
+    meses_de_duracion = models.IntegerField()
+    fecha_de_incorporacion = models.DateField()
+    numero_de_puestos_vacantes = models.IntegerField()
+    horario = models.CharField(choices=enums.HORARIO_DE_TRABAJO)
+    requisitos_de_conocimiento_tecnico = models.ManyToManyField(ConocimientoTecnico,
+                                                                through='RequisitoDeConocimientoTecnico')
+    requisitos_de_experiencia_laboral = models.ManyToManyField(SectorDelMercado, through='RequisitoDeExperienciaLaboral')
+    requisitos_de_idioma = models.ManyToManyField(Idioma, through='RequisitoDeIdioma')
+    # TODO: ubicacion
+
+    # Metadatos de la oferta
+    # TODO: Averiguar como es posible obligar a que las subclases de Oferta implementen
+    # una foreign key a una subclase de Perfil, y el campo de la FK se llame 'autor'
+    fecha_de_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_de_ultima_congelacion = models.DateTimeField()
+    motivo_de_la_congelacion = models.CharField(max_length=140)
+    fecha_de_ultima_modificacion = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+        # TODO: Editar los metadatos de este y el resto de modelos para especificar
+        # un nombre concreto con el que la tabla se guardará en la BD
+
+    @property
+    def tipo_de_jornada(self):
+        if self.horario == 'total':
+            return 'completa'
+        else:
+            return 'parcial'
+
+
+class RequisitoDeConocimientoTecnico(models.Model):
+    oferta = models.ForeignKey(Oferta)
+    conocimiento = models.ForeignKey(ConocimientoTecnico)
+    nivel = models.CharField(choices=enums.NIVEL_DE_CONOCIMIENTO)
+
+
+class RequisitoDeExperienciaLaboral(models.Model):
+    oferta = models.ForeignKey(Oferta)
+    sector = models.ForeignKey(SectorDelMercado)
+    meses = models.IntegerField()
+
+
+class RequisitoDeIdioma(models.Model):
+    oferta = models.ForeignKey(Oferta)
+    idioma = models.ForeignKey(Idioma)
+    nivel = models.CharField(choices=enums.NIVEL_DE_CONOCIMIENTO)
+
+
+class OfertaDeEmpresa(Oferta):
+    # TODO: En este y otros modelos, marcar como ', blank=True, null=True' aquellos campos que puedan quedar vacíos
+    autor = models.ForeignKey(Empresa)
+    ultimo_curso_academico_superado = models.IntegerField(choices=enums.CURSO_ACADEMICO)
+    hay_posibilidad_de_tfg = models.BooleanField()
+    salario_mensual = models.IntegerField()
+    persona_de_contacto = models.CharField()
+    email_de_contacto = models.EmailField()
+
+
+class OfertaDeDepartamento(Oferta):
+    autor = models.ForeignKey(Profesor)
+
+
+class OfertaDeProyectoEmprendedor(Oferta):
+    autor = models.ForeignKey(Estudiante)
