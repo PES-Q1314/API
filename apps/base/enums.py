cf9edf49b285c2260faff409d5882ee95cbb6256
larribas <lorenzo.arribas@me.com>
2014-04-12 14:58:57 +0200

    Implement auxiliary enums, models, and datatypes

---
+++ apps/base/enums.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+SEXO = GENERO = (
+    ('m', 'Masculino'),
+    ('f', 'Femenino'),
+    ('o', 'Otro')
+)
+
+DISPONIBILIDAD = HORARIO_DE_TRABAJO = (
+    ('mañana', 'Mañana'),
+    ('tarde', 'Tarde'),
+    ('total', 'total')
+)
+
+ESTADO_DE_LA_SUSCRIPCION = (
+    ('pendiente', 'Pendiente'),
+    ('aceptada', 'Aceptada'),
+    ('descartada', 'Descartada')
+)
+
+NIVEL_DE_CONOCIMIENTO = (
+    ('medio', 'Medio'),
+    ('avanzado', 'Avanzado'),
+    ('experto', 'Experto')
+)
+
+NIVEL_DE_PRIVACIDAD = (
+    ('privado', 'Privado'),
+    ('protegido', 'Protegido'),
+    ('público', 'Público')
+)
+
+TAMANYO_DE_EMPRESA = (
+    ('pequeña', 'Pequeña'),
+    ('mediana', 'Mediana'),
+    ('grande', 'Grande')
+)
+
+CURSO_ACADEMICO = (
+    (1, 'Primero'),
+    (2, 'Segundo'),
+    (3, 'Tercero'),
+    (4, 'Cuarto'),
+    (5, 'Último'),
+)
--- apps/base/models.py
+++ apps/base/models.py
@@ -1,2 +1,25 @@
+# -*- coding: utf-8 -*-
+from apps.base.datos.idiomas import ISO_639_1
+from django.db import models
 
-# Standard information (knowledge, languages, etc.)
+
+
+class Idioma(models.Model):
+    """ Idioma, siguiendo la ISO 639-1 """
+    codigo = models.CharField(max_length=2, choices=ISO_639_1)
+    idioma = models.CharField(max_length=40)
+
+
+class ConocimientoTecnico(models.Model):
+    conocimiento = models.CharField()
+
+
+class SectorDelMercado(models.Model):
+    sector = models.CharField()
+
+
+class Departamento(models.Model):
+    siglas = models.CharField()
+    nombre = models.CharField()
+    url_upc = models.URLField()
+
