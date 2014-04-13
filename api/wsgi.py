3939710197935c0e7a0a1db5de7e1c9512866d7f
larribas <lorenzo.arribas@me.com>
2014-04-13 18:46:11 +0200

    Prepare for deployment with Heroku

--- api/wsgi.py
+++ api/wsgi.py
@@ -11,4 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
+
+
+
