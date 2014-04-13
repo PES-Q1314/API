6fade941988643cc6183a79b7867a6616bb8361c
larribas <lorenzo.arribas@me.com>
2014-04-13 13:08:00 +0200

    Add fixtures for 'cuentas'

---
+++ apps/cuentas/populate.py
@@ -0,0 +1,6 @@
+from apps.cuentas.models import SystemUser
+
+for i in range(20):
+    u = 'usuario{0}'.format(i)
+    e = '{0}@upc.edu'.format(u)
+    SystemUser.objects.create_user(username=u, email=e, password='1234')
